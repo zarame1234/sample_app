7649def0aa55adf755c8f0092d51aa3db5d0fefc
EC2 Default User <ec2-user@ip-172-31-14-255.ap-northeast-1.compute.internal>
2025-01-20 09:08:56 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
   get '/top' => 'homes#top'
   get 'lists/:id' => 'lists#show', as: 'list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
